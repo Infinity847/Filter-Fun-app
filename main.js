ac1a89bbd109d0e688d2c105942b36d160285c29
Infinity847 <88909782+Infinity847@users.noreply.github.com>
2022-04-29 13:28:50 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,33 @@
+var clown_nose = loadImage('nose.png');
+var noseX; var noseY;
+function setup() {
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
+}
+
+function save_img() {
+  
+}
+
+function draw() {
+
+} 
+function preload() {
+  
+}
+function gotPoses() {
+  console.log('Pose net is starting');
+  if (results.length > 0) {
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    image();
+  }
+}
+function modelLoaded() {
+  console.log('Pose net is working');
+}
